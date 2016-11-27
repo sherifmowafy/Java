d013a9992f1b3a76b55cec73a4a11f3b58616690
Sherif Mowafy <sherifmwafy@gmail.com>
2016-11-27 07:45:57 +0200

    adding Provider render spring project only

---
+++ java/Spring JPassion/Provider-Renderer-Spring/src/main/java/com/sherif/data/provider/DataProvider.java
@@ -0,0 +1,10 @@
+package com.sherif.data.provider;
+
+public class DataProvider implements IDataProvider {
+
+	@Override
+	public String getData() {
+		return "Hello World Data Provider";
+	}
+
+}
---
+++ java/Spring JPassion/Provider-Renderer-Spring/src/main/java/com/sherif/main/Main.java
@@ -0,0 +1,26 @@
+package com.sherif.main;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sherif.data.provider.DataProvider;
+import com.sherif.data.provider.IDataProvider;
+import com.sherif.data.renderer.DataRenderer;
+import com.sherif.data.renderer.IDataRenderer;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BeanFactory factory = new ClassPathXmlApplicationContext("/spring-config.xml");
+
+		IDataProvider dataProvider = (DataProvider) factory.getBean("provider");
+
+		IDataRenderer dataRenderer = (DataRenderer) factory.getBean("renderer");
+
+		dataRenderer.setDataProvider(dataProvider);
+
+		dataRenderer.render();
+	}
+
+}
